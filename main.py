b6784f0b5ae861d8fc875940c2670d93e0525f16
jrddp <jar3dpt@gmail.com>
2021-10-24 18:31:37 -0400

    Reduce button jitter

--- main.py
+++ main.py
@@ -20,13 +20,14 @@ if __name__ == "__main__":
         top_detected = ids is not None and pen_top_aruco_id in ids
         side_detected = ids is not None and any((id in ids for id in pen_side_arcuo_ids))
 
-
         centers = aruco_detector.get_bbox_centers(bboxs)
         true_center = aruco_detector.find_center_of_all_bboxs(bboxs)
 
         if render_video:
             image_drawer.draw_corners(img, bboxs)
             image_drawer.circle_points(img, centers)
+
+        button_down = top_detected and not side_detected
         if true_center is not None:
 
             if render_video:
@@ -34,13 +35,15 @@ if __name__ == "__main__":
 
             screen_point = mouse_controller.img_point_to_mouse_point(img, true_center)
 
-            button_down = top_detected and not side_detected
             mouse_controller.move_mouse(screen_point, button_down=button_down)
-
+        else:
+            mouse_controller.move_mouse((None, None), button_down=button_down)
 
         if render_video:
+            # horizontal flip
             img = cv2.flip(img, 1)
 
+            # draw screen point as text
             if true_center is None:
                 image_drawer.draw_text(img, (100, 100), "Markers not detected")
             else:
--- mouse_controller.py
+++ mouse_controller.py
@@ -4,10 +4,10 @@ import pyautogui
 screen_ratio_x = 0.8
 screen_ratio_y = 0.8
 
+
 def img_point_to_mouse_point(img, img_point):
     img_h, img_w, _ = img.shape
     img_x, img_y = img_point
-
 
     screen_w, screen_h = pyautogui.size()
 
@@ -20,8 +20,6 @@ def img_point_to_mouse_point(img, img_point):
     virtual_ratio_x = min(max(0, (img_x - virtual_edge_w) / virtual_w), 1)
     virtual_ratio_y = min(max(0, (img_y - virtual_edge_h) / virtual_h), 1)
 
-    print(f"{virtual_ratio_x}, {virtual_ratio_y}")
-
     x = screen_w * virtual_ratio_x
     y = screen_h * virtual_ratio_y
 
@@ -32,6 +30,7 @@ def img_point_to_mouse_point(img, img_point):
     y = int(y)
 
     return (x, y)
+
 
 def move_mouse(point, button_down=False):
     x, y = point
